413be7003219a23b9a932d76dc81ff94015d4918
BlaiseMR <blaise-mael.retailleau@hotmail.fr>
2022-02-24 15:46:04 +0100

    update APP

--- My_APII.py
+++ My_APII.py
@@ -113,7 +113,8 @@ def main():
 #     filename = file_selector()
 #     st.write('You selected `%s`' % filename)
     
-    load_pipe = pickle.load(open('./App/Pipe_LogReg/model.pkl', 'rb'))
+#     load_pipe = pickle.load(open('./App/Pipe_LogReg/model.pkl', 'rb'))
+    load_pipe = pickle.load(open('./App/Pipe_XGBoost/model.pkl', 'rb'))
     MLB = pickle.load(open('./App/LabelBinarizer.pkl', 'rb'))
 
     st.title('Find Best tags for your Stackoverflow question')
